49910d86c08dd70f8d2b0ac98c75ed4d1defd3b3
NguyenVanViet <it.nguyenvanviet@gmail.com>
2022-05-21 18:14:05 +0700

    commit

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,24 +1,27 @@
-import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classNames from "classnames/bind";
-import styles from './AccountItem.module.scss'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
 
-
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 function AccountItem() {
-    return <div className={cx('wrapper')}>
-        <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/0d21a9ad0530420a03469cad4fdf7f12~c5_300x300.webp?x-expires=1653292800&x-signature=O4ZkRgFo66Dsk8PnXPUE99T9Hvc%3D" alt="" />
-        <div className={cx('info')}>
-            <h4 className={cx('name')}>
-                <span className={cx('')}>
-                    Nguyen van A
-                </span>
-                <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-            </h4>
-            <span className={cx('username')}>Nguyen Van A</span>
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/0d21a9ad0530420a03469cad4fdf7f12~c5_300x300.webp?x-expires=1653292800&x-signature=O4ZkRgFo66Dsk8PnXPUE99T9Hvc%3D"
+                alt=""
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span className={cx('')}>Nguyen van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>Nguyen Van A</span>
+            </div>
         </div>
-    </div>;
+    );
 }
 
 export default AccountItem;
---
+++ src/components/Button/index.js
@@ -0,0 +1,70 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    small = false,
+    large = false,
+    rounded = false,
+    disabled = false,
+    children,
+    className,
+    leftIcon,
+    RightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+
+    const props = {
+        onClick,
+        ...passProps,
+    };
+
+    // Remove event listener when btn is disabled
+    if (disabled) {
+        // c1:
+        // delete props.onClick;
+        // c2
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        text,
+        rounded,
+        disabled,
+        small,
+        large,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {RightIcon && <span className={cx('icon')}>{RightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
