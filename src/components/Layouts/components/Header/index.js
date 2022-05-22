9bbef9280bfe9203ec7d28c0219c4e47ae3736b8
NguyenVanViet <it.nguyenvanviet@gmail.com>
2022-05-22 17:39:27 +0700

    commit

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,15 +1,40 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+    faCircleXmark,
+    faEarthAsia,
+    faEllipsisVertical,
+    faKeyboard,
+    faMagnifyingGlass,
+    faQuestionCircle,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Tippy from '@tippyjs/react/headless';
-
+import AccountItem from '~/components/AccountItem';
 import Button from '~/components/Button';
+import Menu from '~/components/Popper/Menu';
+
 import styles from './Header.module.scss';
 import images from '~/assets/images';
-import AccountItem from '~/components/AccountItem';
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faQuestionCircle} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard Shotcuts',
+    },
+];
 
 function Header() {
     const [searchResult, setSearchResult] = useState([]);
@@ -54,6 +79,12 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Login</Button>
+
+                    <Menu items={MENU_ITEMS}>
+                        <button className={cx('more-btn')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical} />
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -1,11 +1,9 @@
-import classNames from "classnames/bind";
-import styles from './Popper.module.scss'
+import classNames from 'classnames/bind';
+import styles from './Popper.module.scss';
 
-const cx = classNames.bind(styles)
-function Wrapper({ children }) {
-    return <div className={cx('wrapper')}>
-        { children }
-    </div>;
+const cx = classNames.bind(styles);
+function Wrapper({ children, className }) {
+    return <div className={cx('wrapper', className)}>{children}</div>;
 }
 
 export default Wrapper;
